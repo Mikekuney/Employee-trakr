9ead35dc5ba0a0a9d5f16dffef7f7c4dc2ffd56e
Mikekuney <96913841+Mikekuney@users.noreply.github.com>
2022-05-07 09:27:15 -0400

    added schema.sql and tables

---
+++ schema.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS Culinary_db;
+
+CREATE DATABASE Culinary_db;
+
+USE Culinary_db;
+
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    dep_names VARCHAR(30) NOT NULL
+):
+
+CREATE TABLE roles (
+    r_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(8, 2) NOT NULL,
+    department_id INT,
+    CONSTRAINT fk_deparment FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    emp_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    roles_id INT NOT NULL,
+        CONSTRAINT fk_role FOREIGN KEY (roles_id) REFERENCES roles(r_id) ON DELETE CASCADE,
+    manager_id INT,
+        CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(emp_id) ON DELETE SET NULL
+);
+
+SELECT*FROM departments;
+
+SELECT*FROM roles;
+
+SELECT*FROM employee;
+
