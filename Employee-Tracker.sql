f85ed33ff8347d423391bd4d56ee3b979bb0d465
Mikekuney <96913841+Mikekuney@users.noreply.github.com>
2022-05-09 09:15:09 -0400

    added connection and start function to Employee-Tracker.sql

---
+++ Employee-Tracker.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS EMS_db;
+
+CREATE DATABASE EMS_db;
+
+USE EMS_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    NAMES VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    r_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(8, 2) NOT NULL,
+    department_id INT NOT NULL,
+	FOREIGN KEY (r_id) REFERENCES employee(emp_id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    emp_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    roles_id INT NOT NULL,
+    manager_id INT,
+	CONSTRAINT fk_role FOREIGN KEY  (roles_id) REFERENCES roles(r_id) ON DELETE CASCADE,
+	CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES  employee(emp_id) ON DELETE SET NULL
+);
+
+
+SELECT*FROM departments;
+
+SELECT*FROM roles;
+
+SELECT*FROM employees;
